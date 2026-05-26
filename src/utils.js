f5bb86f08620a8d71fc9959db0ee28b5e49deb28
OpsCraft <Afzal@afzaldosani.in>
2026-05-26 16:47:49 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -68,7 +68,7 @@ export function waLink(phone) {
   if (digits.length === 10) {
     normalized = '91' + digits
   }
-  return `https://wa.me/${normalized}`
+  return `https://web.whatsapp.com/send?phone=${normalized}`
 }
 
 export function exportCSV(clients) {
